0c04093494ec3ec0dcf45831542ac2ab978818a0
Ponniah96 <ponniah@DEV-LPT324.local>
2024-10-17 14:05:11 +0530

    Updated HomeScreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatListModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, MatListModule],
 })
 export class AppComponent {
   title = 'my-app';
