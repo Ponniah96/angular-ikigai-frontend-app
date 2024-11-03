55f9474fbb362e6cc3a172462896492ef7a162fc
Ponniah96 <ponniah@DEV-LPT324.local>
2024-11-04 01:19:49 +0530

    Created Header Layout along with API Integration

--- src/app/core/services/navigation/navigation.service.ts
+++ src/app/core/services/navigation/navigation.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
 import { NavMenu } from "../../models/navigation/navigation.model";
 import { NavTeamMenu } from "../../models/navigation/navigation.model";
+import { APIResponse } from "../../models/navigation/navigation.model";
 import { NavMenuDataResponse } from "../../../data/responses/navigation/nav-data.response";
 import {NavInidividualTeamResponse} from "../../../data/responses/navigation/nav-data.response";
 
@@ -24,3 +27,19 @@ export class NavigationTeamMenuService {
     return NavInidividualTeamResponse.teamList;
   }
 }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationAPIService {
+  private apiURL = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http:HttpClient) {}
+  getAPIData(): Observable<APIResponse[]> {
+    console.log("API Data",this.http.get<APIResponse[]>(this.apiURL));
+    this.http.get(this.apiURL).subscribe((data) => {
+      console.log("API Data",data);
+    });
+    return this.http.get<APIResponse[]>(this.apiURL);
+  }
+}
+
--- src/app/domain/use-cases/navigation/navigation-data.use-case.ts
+++ src/app/domain/use-cases/navigation/navigation-data.use-case.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { NavMenu } from "../../../core/models/navigation/navigation.model";
 import { NavTeamMenu } from "../../../core/models/navigation/navigation.model";
+import { APIResponse } from "../../../core/models/navigation/navigation.model";
 import { NavigationMenuService } from "../../../core/services/navigation/navigation.service";
 import { NavigationTeamMenuService } from "../../../core/services/navigation/navigation.service";
+import { NavigationAPIService } from "../../../core/services/navigation/navigation.service";
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +25,16 @@ export class NavigationTeamMenuDataUseCase {
   constructor( private navigationTeamMenuService: NavigationTeamMenuService) { }  
   execute(): NavTeamMenu[] {
     return this.navigationTeamMenuService.getNavTeamMenuData();
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationAPIDataUseCase {
+  constructor(private navigationAPIService: NavigationAPIService) { }
+  execute(): Observable<APIResponse[]> {
+    console.log("API Data",this.navigationAPIService.getAPIData());
+    return this.navigationAPIService.getAPIData();
   }
 }
